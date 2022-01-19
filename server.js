8a40f554e21c979f7d701dabd1d41d09f0e7646a
Leticia Corral <leticiacorral@Leticias-MacBook-Air.local>
2022-01-19 18:17:30 -0300

    Configurando build heroku

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require("express");
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
+const express = require('express');
 const app = express();
 
-app.use(express.static("./dist/ecommerceMaskara/src"));
+app.use(requireHTTPS);
+app.use(express.static('./dist/ecommerce'));
 
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/ecommerceMaskara/src" });
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ecommerce/'}),
+);
 
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`);
