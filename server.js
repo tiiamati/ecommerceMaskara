5fca48ddf0358be289c7c4566b1ce62cfe5f9167
Leticia Corral <leticiacorral@Leticias-MacBook-Air.local>
2022-01-19 17:47:26 -0300

    Configurando build heroku

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 
 app.get('*', function (req, res) {
-    res.sendFile('./index.html');
-});
+    const index = path.join(__dirname, 'app', 'index.html');
+    res.sendFile(index);
+  });
 
 app.listen(process.env.PORT || 8080);
